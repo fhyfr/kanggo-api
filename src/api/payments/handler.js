1722b22fcafe2cd3cdadc85f2dfcc71ec8bda6aa
Firman <fhaya.firman@gmail.com>
2021-09-10 11:41:53 +0700

    create payments plugin

--- src/api/payments/handler.js
+++ src/api/payments/handler.js
@@ -6,30 +6,62 @@ class PaymentsHandler {
     this.validator = validator;
 
     this.postPaymentHandler = this.postPaymentHandler.bind(this);
+    this.getPaymentByIdHandler = this.getPaymentByIdHandler.bind(this);
+    this.putPaymentByIdHandler = this.putPaymentByIdHandler.bind(this);
   }
 
   async postPaymentHandler(request, h) {
     try {
-      this.validator.validatePaymentPayload(request.payload);
+      this.validator.validatePostPaymentPayload(request.payload);
 
-      const { id: credentialId } = request.auth.credentials;
       const { order_id: orderId, amount } = request.payload;
-
       const paymentId = await this.service.addPayment({
-        credentialId,
         orderId,
         amount,
       });
 
       const response = h.response({
         status: 'success',
-        message: 'Pembayaran berhasil',
+        message: 'Payment berhasil ditambahkan',
         data: {
           paymentId,
         },
       });
       response.code(201);
       return response;
+    } catch (error) {
+      return errorHandler(error, h);
+    }
+  }
+
+  async getPaymentByIdHandler(request, h) {
+    try {
+      const { id: paymentId } = request.params;
+      const payment = await this.service.getPaymentById(paymentId);
+
+      return {
+        status: 'success',
+        data: {
+          payment,
+        },
+      };
+    } catch (error) {
+      return errorHandler(error, h);
+    }
+  }
+
+  async putPaymentByIdHandler(request, h) {
+    try {
+      this.validator.validatePutPaymentPayload(request.payload);
+
+      const { payment_id: paymentId, amount } = request.payload;
+
+      await this.service.processPayment(paymentId, amount);
+
+      return {
+        status: 'success',
+        message: 'Pembayaran berhasil diproses',
+      };
     } catch (error) {
       return errorHandler(error, h);
     }
